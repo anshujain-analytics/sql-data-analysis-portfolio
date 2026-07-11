c68883394084097bc3b97027ccfee1c8ca1202c9
Anshu Jain <jainanshu60@gmail.com>
2026-07-12 04:37:34 +0530

    Add AGENTS and CUSTOMER tables with sample data
    Created AGENTS and CUSTOMER tables with initial data.

--- Customer-Agent-Analysis/create_tables.sql
+++ Customer-Agent-Analysis/create_tables.sql
@@ -1,1 +1,56 @@
+create table AGENTS (
+  "AGENT_CODE" VARCHAR NOT NULL PRIMARY KEY,
+  "AGENT_NAME" VARCHAR,
+  "WORKING_AREA" VARCHAR,
+  "COMMISSION" FLOAT,
+  "PHONENO" VARCHAR,
+  "COUNTRY" VARCHAR
+  );
+INSERT INTO AGENTS VALUES ('A001', 'Ramasundar', 'Bangalore', '0.15', '07725814763', 'India');   
+INSERT INTO AGENTS VALUES ('A003', 'Alex ', 'London', '0.13', '075-12458969', 'UK');  
+INSERT INTO AGENTS VALUES ('A008', 'Alford', 'New York', '0.12', '044-25874365', 'USA');  
+INSERT INTO AGENTS VALUES ('A011', 'Ravi Kumar', 'Bangalore', '0.15', '07745625874', 'India');  
+INSERT INTO AGENTS VALUES ('A004', 'Ramasundar', 'Bangalore', '0.15', '07725814763', 'Sri Lanka');
+
+
+CREATE TABLE  CUSTOMER  (
+CUST_ID VARCHAR NOT NULL PRIMARY KEY,  
+CUST_NAME VARCHAR NOT NULL,  
+CUST_CITY VARCHAR,  
+WORKING_AREA VARCHAR NOT NULL,  
+CUST_COUNTRY VARCHAR NOT NULL,  
+GRADE INT,  
+OPENING_AMT FLOAT NOT NULL,  
+RECEIVE_AMT FLOAT NOT NULL,  
+PAYMENT_AMT FLOAT NOT NULL,   
+OUTSTANDING_AMT FLOAT NOT NULL,  
+PHONE_NO VARCHAR NOT NULL,  
+Emp_ID  VARCHAR NOT NULL ,
+Agent Code VARCHAR
+);  
+INSERT INTO CUSTOMER VALUES ('C00013', 'Holmes', 'London', 'London', 'UK', '2', '6000.00', '5000.00', '7000.00', '4000.00', 'BBBBBBB', '1','A008'); 
+INSERT INTO CUSTOMER VALUES ('C00001', 'Micheal', 'New York', 'New York', 'USA', '2', '3000.00', '5000.00', '2000.00', '6000.00', 'CCCCCCC', '2', 'A001'); 
+INSERT INTO CUSTOMER VALUES ('C00020', 'Albert', 'New York', 'New York', 'USA', '3', '5000.00', '7000.00', '6000.00', '6000.00', 'BBBBSBB', '3', 'A008'); 
+INSERT INTO CUSTOMER VALUES ('C00025', 'Ravindran', 'Bangalore', 'Bangalore', 'India', '2', '5000.00', '7000.00', '4000.00', '8000.00', 'AVAVAVA', '4', 'A011'); 
+INSERT INTO CUSTOMER VALUES ('C00024', 'Cook', 'London', 'London', 'UK', '2', '4000.00', '9000.00', '7000.00', '6000.00', 'FSDDSDF', '5', 'A008'); 
+INSERT INTO CUSTOMER VALUES ('C00015', 'Stuart', 'London', 'London', 'UK', '1', '6000.00', '8000.00', '3000.00', '11000.00', 'GFSGERS', '6', 'A003'); 
+INSERT INTO CUSTOMER VALUES ('C00002', 'Bolt', 'New York', 'New York', 'USA', '3', '5000.00', '7000.00', '9000.00', '3000.00', 'DDNRDRH', '7', 'A003'); 
+INSERT INTO CUSTOMER VALUES ('C00018', 'Fleming', 'Brisban', 'Brisban', 'Australia', '2', '7000.00', '7000.00', '9000.00', '5000.00', 'NHBGVFC', '8', 'A001'); 
+INSERT INTO CUSTOMER VALUES ('C00021', 'Jacks', 'Brisban', 'Brisban', 'Australia', '1', '7000.00', '7000.00', '7000.00', '7000.00', 'WERTGDF', '9', 'A004'); 
+INSERT INTO CUSTOMER VALUES ('C00019', 'Yearannaidu', 'Chennai', 'Chennai', 'India', '1', '8000.00', '7000.00', '7000.00', '8000.00', 'ZZZZBFV', '10', 'A003'); 
+INSERT INTO CUSTOMER VALUES ('C00005', 'Sasikant', 'Mumbai', 'Mumbai', 'India', '1', '7000.00', '11000.00', '7000.00', '11000.00', '147-25896312', '1', 'A001'); 
+INSERT INTO CUSTOMER VALUES ('C00007', 'Ramanathan', 'Chennai', 'Chennai', 'India', '1', '7000.00', '11000.00', '9000.00', '9000.00', 'GHRDWSD', '2', 'A008'); 
+INSERT INTO CUSTOMER VALUES ('C00022', 'Avinash', 'Mumbai', 'Mumbai', 'India', '2', '7000.00', '11000.00', '9000.00', '9000.00', '11312345678','3', 'A001'); 
+INSERT INTO CUSTOMER VALUES ('C00004', 'Winston', 'Brisban', 'Brisban', 'Australia', '1', '5000.00', '8000.00', '7000.00', '6000.00', 'AAAAAAA', '4', 'A004'); 
+INSERT INTO CUSTOMER VALUES ('C00023', 'Karl', 'London', 'London', 'UK', '0', '4000.00', '6000.00', '7000.00', '3000.00', 'AAAABAA', '6', 'A003'); 
+INSERT INTO CUSTOMER VALUES ('C00006', 'Shilton', 'Torento', 'Torento', 'Canada', '1', '10000.00', '7000.00', '6000.00', '11000.00', 'DDDDDDD', '10', 'A003'); 
+INSERT INTO CUSTOMER VALUES ('C00010', 'Charles', 'Hampshair', 'Hampshair', 'UK', '3', '6000.00', '4000.00', '5000.00', '5000.00', 'MMMMMMM', '11', 'A003'); 
+INSERT INTO CUSTOMER VALUES ('C00017', 'Srinivas', 'Bangalore', 'Bangalore', 'India', '2', '8000.00', '4000.00', '3000.00', '9000.00', 'AAAAAAB', '6', 'A004'); 
+INSERT INTO CUSTOMER VALUES ('C00012', 'Steven', 'San Jose', 'San Jose', 'USA', '1', '5000.00', '7000.00', '9000.00', '3000.00', 'KRFYGJK', '7', 'A011'); 
+INSERT INTO CUSTOMER VALUES ('C00008', 'Karolina', 'Torento', 'Torento', 'Canada', '1', '7000.00', '7000.00', '9000.00', '5000.00', 'HJKORED', '2', 'A011'); 
+INSERT INTO CUSTOMER VALUES ('C00003', 'Martin', 'Torento', 'Torento', 'Canada', '2', '8000.00', '7000.00', '7000.00', '8000.00', 'MJYURFD', '6', 'A004'); 
+INSERT INTO CUSTOMER VALUES ('C00009', 'Ramesh', 'Mumbai', 'Mumbai', 'India', '3', '8000.00', '7000.00', '3000.00', '12000.00', 'Phone No', '9', 'A001'); 
+INSERT INTO CUSTOMER VALUES ('C00014', 'Rangarappa', 'Bangalore', 'Bangalore', 'India', '2', '8000.00', '11000.00', '7000.00', '12000.00', 'AAAATGF', '5', 'A001'); 
+INSERT INTO CUSTOMER VALUES ('C00016', 'Venkatpati', 'Bangalore', 'Bangalore', 'India', '2', '8000.00', '11000.00', '7000.00', '12000.00', 'JRTVFDD', '2', 'A008'); 
+INSERT INTO CUSTOMER VALUES ('C00011', 'Sundariya', 'Chennai', 'Chennai', 'India', '3', '7000.00', '11000.00', '7000.00', '11000.00', 'PPHGRTS', '8', 'A008');
 
