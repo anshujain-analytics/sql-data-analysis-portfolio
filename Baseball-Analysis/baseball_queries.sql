6f5a0c2b04c7acd705a29f21e21cc6665feb8dfc
Anshu Jain <jainanshu60@gmail.com>
2026-07-12 04:16:30 +0530

    Add SQL queries for player height and weight analysis

--- Baseball-Analysis/baseball_queries.sql
+++ Baseball-Analysis/baseball_queries.sql
@@ -1,1 +1,8 @@
+Q 1 - Who are the top 5 players having height more than average?
+--SELECT * FROM baseball_data;
+--SELECT avg(height) from baseball_data;
+--SELECT name, height FROM baseball_data where height > (SELECT CAST(avg(height) as Float) from baseball_data) ORDER BY height DESC LIMIT 5;
+  
 
+Q2 - Bottom 2 players having least weight and must be left handed?
+SELECT * from baseball_data WHERE handedness = 'L' ORDER BY weight ASC;
